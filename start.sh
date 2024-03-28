c0444ad06232e63c0d5b7b7f25f19a5f81ee6104
Werevart <160262233+Wrevart@users.noreply.github.com>
2024-03-29 00:16:53 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-source ~/.bash_profile, source ~/.bashrc, source ~/.zshrc
+source ~/.bashrc
 
 nvm install node
 nvm install 18
