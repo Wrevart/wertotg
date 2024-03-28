31a1cfcf01c74e842edaa51317d7308bf88fe6ab
Werevart <160262233+Wrevart@users.noreply.github.com>
2024-03-28 23:49:13 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,13 +4,12 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm install node
 apt install npm
 
-cd /root/wertotg/
 npm install axios
 npm install axios cheerio
 
-node peerskjnodes.js
+node /root/wertotg/peerskjnodes.js
 
-node peersnodejumper.js
+node /root/wertotg/peersnodejumper.js
 PEERS=$"(cat peers.txt)"
 sed -i 's|^persistent_peers =.|persistent_peers = "'$PEERS'"|' $HOME/.babylond/config/config.toml
 cd 
