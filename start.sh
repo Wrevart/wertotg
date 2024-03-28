79057564f38ae6bf023fc340e25848ccf9a08055
Werevart <160262233+Wrevart@users.noreply.github.com>
2024-03-28 23:53:31 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,10 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm install node
 apt install npm
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 npm install axios
 npm install axios cheerio
 
